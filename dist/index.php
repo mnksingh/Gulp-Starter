f695f75515d5580b45fa3a5e2261fd2324821614
msfartiyal <msfartiyal@craterzone.com>
2019-06-01 23:06:38 +0530

    added support for sass with postcss and php support as well

---
+++ dist/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+    />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/styles.css" type="text/css" />
+  </head>
+
+  <body>
+    <button>Click here</button>
+    <script src="assets/js/script.js"></script>
+  </body>
+</html>
--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,62 @@
-/* eslint-env node */
 "use strict";
 
+const MAIN_PORT = 3000;
+
+const PHP_PORT = 8001;
+
 const gulp = require("gulp"),
-      plumber = require("gulp-plumber"),
-      browsersync = require("browser-sync").create(),
-      sourcemaps = require("gulp-sourcemaps");
+  plumber = require("gulp-plumber"),
+  browsersync = require("browser-sync"),
+  sourcemaps = require("gulp-sourcemaps");
+
+const phpConnect = require("gulp-connect-php");
+
+const sass = require("gulp-sass");
 
 const postcss = require("gulp-postcss"),
-      postcssPresetEnv = require("postcss-preset-env"),
-      lost = require("lost"),
-      cssnano = require('cssnano'),
-      precss = require("precss"),
-      atImport = require("postcss-import"),
-      autoprefixer = require("autoprefixer"),
-      rucksack = require('rucksack-css'),
-      mqpacker = require("css-mqpacker");
+  postcssPresetEnv = require("postcss-preset-env"),
+  lost = require("lost"),
+  cssnano = require("cssnano"),
+  rucksack = require("rucksack-css"),
+  mqpacker = require("css-mqpacker");
 
 const Buffer = require("vinyl-buffer"),
-      source = require("vinyl-source-stream"),
-      babelify = require("babelify"),
-      browserify = require("browserify"),
-      uglify = require("gulp-uglify");
+  source = require("vinyl-source-stream"),
+  babelify = require("babelify"),
+  browserify = require("browserify"),
+  uglify = require("gulp-uglify");
 
 const globals = {
+  distDir: "./dist",
   js: {
     src: "src/js/script.js",
     watch: "src/js/**.*js",
     dest: "dist/assets/js"
   },
   css: {
-    src: "src/postcss/main.css",
-    watch: "src/postcss/**/*.css",
+    src: "src/sass/styles.scss",
+    watch: "src/sass/**/*.scss",
     dest: "dist/assets/css/"
   },
   postcssPlugins: [
-      atImport(),
-      postcssPresetEnv({
-        autoprefixer: {
-          browsers: ['IE >= 9']
-        }
-      }),
-      precss(),
-      rucksack(),
-      lost(),
-      mqpacker(),
-      cssnano()
+    postcssPresetEnv({
+      autoprefixer: {
+        browsers: ["> 1%", "last 2 versions", "IE 10"]
+      }
+    }),
+    rucksack(),
+    lost(),
+    mqpacker(),
+    cssnano()
   ]
-}
+};
 
-function styles () {
+function styles() {
   return gulp
     .src(globals.css.src)
     .pipe(plumber())
     .pipe(sourcemaps.init())
+    .pipe(sass())
     .pipe(postcss(globals.postcssPlugins))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(globals.css.dest))
@@ -71,49 +75,59 @@ function scripts() {
     entries: globals.js.src,
     debug: true,
     // defining transforms here will avoid crashing your stream
-    transform: [babelify.configure({
-      presets: ['env']
-    })]
+    transform: [
+      babelify.configure({
+        presets: ["@babel/preset-env"]
+      })
+    ]
   });
 
-  return b.bundle()
-     .pipe(source('script.js'))
-     .pipe(Buffer())
-     .pipe(sourcemaps.init({loadMaps: true}))
-         // Add transformation tasks to the pipeline here.
-         .pipe(uglify())
-         .on('error',  console.error.bind(console))
-     .pipe(sourcemaps.write('.'))
-     .pipe(gulp.dest(globals.js.dest))
-     .pipe(browsersync.stream());
+  return (
+    b
+      .bundle()
+      .pipe(source("script.js"))
+      .pipe(Buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      // Add transformation tasks to the pipeline here.
+      .pipe(uglify())
+      .on("error", console.error.bind(console))
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest(globals.js.dest))
+      .pipe(browsersync.stream())
+  );
 }
 
 function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./dist",
-      online: false,
+  phpConnect.server(
+    {
+      base: globals.distDir,
+      port: PHP_PORT
     },
-    port: 3000
-  });
-  done();
+    () => {
+      browsersync({
+        proxy: `http://localhost:${PHP_PORT}`,
+        notify: false,
+        open: true,
+        port: MAIN_PORT
+      });
+      done();
+    }
+  );
 }
-
-function browserSyncReload(done) {
-  browsersync.reload();
-}
-
-gulp.task('css', styles);
-gulp.task('js', scripts);
-gulp.task("browser-sync", browserSync);
-gulp.task('build', gulp.parallel('js', 'css'));
 
 function watchFiles() {
-  gulp.watch("dist/**/*.html").on("change", browserSyncReload);
+  gulp.watch("dist/**/*.html").on("change", browsersync.reload);
+  gulp.watch("dist/**/*.php").on("change", browsersync.reload);
   gulp.watch(globals.js.watch, scripts);
   gulp.watch(globals.css.watch, styles);
 }
 
+gulp.task("css", styles);
+gulp.task("js", scripts);
+
+gulp.task("browser-sync", browserSync);
+gulp.task("build", gulp.parallel("js", "css"));
+
 gulp.task("watch", watchFiles);
 
-gulp.task('default', gulp.series('build', 'browser-sync', 'watch'));
+gulp.task("default", gulp.series("build", "browser-sync", "watch"));
